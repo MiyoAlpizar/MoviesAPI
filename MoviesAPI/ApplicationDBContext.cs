24b457e5331aff994fbbc8c5258f0ffcbd7c1245
miyoalpizar <Miyo@DESKTOP-VOHF5HE>
2021-03-31 17:36:05 -0600

    Add project files.

---
+++ MoviesAPI/ApplicationDBContext.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.EntityFrameworkCore;
+using MoviesAPI.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MoviesAPI
+{
+    public class ApplicationDBContext : DbContext
+    {
+        public ApplicationDBContext(DbContextOptions options) : base(options)
+        {
+
+        }
+
+        public DbSet<Gender> Genders { get; set; }
+
+
+    }
+}
---
+++ MoviesAPI/Controllers/GendersController.cs
@@ -0,0 +1,34 @@
+﻿using AutoMapper;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using MoviesAPI.DTOs;
+using MoviesAPI.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MoviesAPI.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class GendersController : ControllerBase
+    {
+        private readonly ApplicationDBContext context;
+        private readonly IMapper mapper;
+
+        public GendersController(ApplicationDBContext context, IMapper mapper)
+        {
+            this.context = context;
+            this.mapper = mapper;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<List<GenderDTO>>> Get()
+        {
+            var genders = await context.Genders.ToListAsync();
+            return mapper.Map<List<GenderDTO>>(genders);
+        }
+
+    }
+}
